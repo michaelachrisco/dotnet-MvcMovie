484bfcc17367623794689f3cf55a4c5d9a5d29b3
MichaelAChrisco <michaelachrisco@gmail.com>
2016-08-25 18:01:41 -0700

    Basic filter

--- Models/Movie.cs
+++ Models/Movie.cs
@@ -1,13 +1,42 @@
 using System;
+using System.ComponentModel.DataAnnotations;
+
+using MvcMovie.CustomAttributes;
 
 namespace MvcMovie.Models
 {
-    public class Movie
+    public class Movie : ClassicMovieAttribute
     {
         public int ID { get; set; }
+        [Required]
+        [StringLength(100)]
         public string Title { get; set; }
+        [DisplayFormatAttribute(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
+        [DisplayAttribute(Name = "Classic")]
+        [ClassicMovie(1960)]
         public DateTime ReleaseDate { get; set; }
-        public string Genre { get; set; }
+
+        [Required]
+        [StringLength(1000)]
+        public string Description { get; set; }
+
+        [Required]
+        [Range(0, 999.99)]
         public decimal Price { get; set; }
+
+        [Required]
+        public Genre Genre { get; set; }
+
+        public bool Preorder { get; set; }
+    }
+
+        public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
+    {
+        if (Genre == Genre.Classic && ReleaseDate.Year > _classicYear)
+        {
+            yield return new ValidationResult(
+                "Classic movies must have a release year earlier than " + _classicYear,
+                new[] { "ReleaseDate" });
+        }
     }
 }
